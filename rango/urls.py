2c375c7b63d8608ca1c4b8cf94bd6bae0f4803f2
Vivekasr <hie.vivi.sharma@gmail.com>
2019-03-02 12:50:09 +0530

    its mess

--- rango/urls.py
+++ rango/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from rango import views
+from django.views.static import serve
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name = 'index')
-]
+    path('', views.index, name = 'index'),
+    path('media', serve, {'document_root': settings.MEDIA_ROOT}),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT,)
